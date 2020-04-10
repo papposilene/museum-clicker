a073b2b049a91632c25c974ad95bf8483d26d062
papposilene <papposilene@users.noreply.github.com>
2020-04-10 11:53:34 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@
   var game = new Game.Game();
   game.load();
 
-  var lab = game.lab;
-  var research = game.research;
-  var workers = game.workers;
+  var museum = game.museum;
+  var collection = game.collection;
+  var curators = game.curators;
   var upgrades = game.upgrades;
   var achievements = game.achievements;
   var allObjects = game.allObjects;
   var lastSaved;
 
-  var app = angular.module('particleClicker', []);
+  var app = angular.module('museumClicker', []);
 
   app.filter('niceNumber', ['$filter', function($filter) {
       return Helpers.formatNumberPostfix;
@@ -35,57 +35,57 @@
     };
   }]);
 
-  app.controller('DetectorController', function() {
+  app.controller('TicketOfficeController', function() {
     this.click = function() {
-      lab.clickDetector();
-      detector.addEvent();
-      UI.showUpdateValue("#update-data", lab.state.detector);
+      museum.clickDetector();
+      ticketoffice.addEvent();
+      UI.showUpdateValue("#update-artwork", museum.state.ticketoffice);
       return false;
     };
   });
 
   // Hack to prevent text highlighting
-  document.getElementById('detector').addEventListener('mousedown', function(e) {
+  document.getElementById('ticketoffice').addEventListener('mousedown', function(e) {
     e.preventDefault();
   });
 
-  app.controller('LabController', ['$interval', function($interval) {
-    this.lab = lab;
-    this.showDetectorInfo = function() {
-      if (!this._detectorInfo) {
-        this._detectorInfo = Helpers.loadFile('html/detector.html');
+  app.controller('MuseumController', ['$interval', function($interval) {
+    this.museum = museum;
+    this.showTicketInfo = function() {
+      if (!this._ticketInfo) {
+        this._ticketInfo = Helpers.loadFile('html/ticketoffice.html');
       }
-      UI.showModal('Detector', this._detectorInfo);
+      UI.showModal('TicketOffice', this._ticketInfo);
     };
     $interval(function() {  // one tick
-      var grant = lab.getGrant();
-      UI.showUpdateValue("#update-funding", grant);
+      var grant = museum.getGrant();
+      UI.showUpdateValue("#update-cashflow", grant);
       var sum = 0;
-      for (var i = 0; i < workers.length; i++) {
-        sum += workers[i].state.hired * workers[i].state.rate;
+      for (var i = 0; i < curators.length; i++) {
+        sum += curators[i].state.hired * curators[i].state.rate;
       }
       if (sum > 0) {
-        lab.acquireData(sum);
-        UI.showUpdateValue("#update-data", sum);
-        detector.addEventExternal(workers.map(function(w) {
+        museum.acquireData(sum);
+        UI.showUpdateValue("#update-artwork", sum);
+        ticketoffice.addEventExternal(curators.map(function(w) {
           return w.state.hired;
         }).reduce(function(a, b){return a + b}, 0));
       }
     }, 1000);
   }]);
 
-  app.controller('ResearchController', ['$compile', function($compile) {
-    this.research = research;
+  app.controller('CollectionController', ['$compile', function($compile) {
+    this.collection = collection;
     this.isVisible = function(item) {
-      return item.isVisible(lab);
+      return item.isVisible(museum);
     };
     this.isAvailable = function(item) {
-      return item.isAvailable(lab);
+      return item.isAvailable(museum);
     };
     this.doResearch = function(item) {
-      var cost = item.research(lab);
+      var cost = item.collection(museum);
       if (cost > 0) {
-        UI.showUpdateValue("#update-data", -cost);
+        UI.showUpdateValue("#update-artwork", -cost);
         UI.showUpdateValue("#update-reputation", item.state.reputation);
       }
     };
@@ -96,17 +96,17 @@
   }]);
 
   app.controller('HRController', function() {
-    this.workers = workers;
+    this.curators = curators;
     this.isVisible = function(worker) {
-      return worker.isVisible(lab);
+      return worker.isVisible(museum);
     };
     this.isAvailable = function(worker) {
-      return worker.isAvailable(lab);
+      return worker.isAvailable(museum);
     };
     this.hire = function(worker) {
-      var cost = worker.hire(lab);
+      var cost = worker.hire(museum);
       if (cost > 0) {
-        UI.showUpdateValue("#update-funding", -cost);
+        UI.showUpdateValue("#update-cashflow", -cost);
       }
     };
   });
@@ -114,14 +114,14 @@
   app.controller('UpgradesController', function() {
     this.upgrades = upgrades;
     this.isVisible = function(upgrade) {
-      return upgrade.isVisible(lab, allObjects);
+      return upgrade.isVisible(museum, allObjects);
     };
     this.isAvailable = function(upgrade) {
-      return upgrade.isAvailable(lab, allObjects);
+      return upgrade.isAvailable(museum, allObjects);
     };
     this.upgrade = function(upgrade) {
-      if (upgrade.buy(lab, allObjects)) {
-        UI.showUpdateValue("#update-funding", upgrade.cost);
+      if (upgrade.buy(museum, allObjects)) {
+        UI.showUpdateValue("#update-cashflow", upgrade.cost);
       }
     }
   });
@@ -129,7 +129,7 @@
   app.controller('AchievementsController', function($scope) {
     $scope.achievements = achievements;
     $scope.progress = function() {
-      return achievements.filter(function(a) { return a.validate(lab, allObjects, lastSaved); }).length;
+      return achievements.filter(function(a) { return a.validate(museum, allObjects, lastSaved); }).length;
     };
   });
 
@@ -139,7 +139,7 @@
     $scope.lastSaved = lastSaved;
     $scope.saveNow = function() {
       var saveTime = new Date().getTime();
-      game.lab.state.time += saveTime - lastSaved;
+      game.museum.state.time += saveTime - lastSaved;
       game.save();
       lastSaved = saveTime;
       $scope.lastSaved = lastSaved;
@@ -156,7 +156,7 @@
   }]);
 
   app.controller('StatsController', function($scope) {
-    $scope.lab = lab;
+    $scope.museum = museum;
   });
 
   analytics.init();
